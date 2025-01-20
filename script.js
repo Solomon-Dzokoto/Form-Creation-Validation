9b89ed8a152bd2abceb76776d16ca08629ac1cc9
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-01-20 20:55:31 +0000

    modifiesd the fuction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",function (){
 const form = document.getElementById('registration-form')
 const feedbackDiv = document.getElementById('form-feedback')
 
 console.log(form)
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit",function (e){
    e.preventDefault();
    const username = document.getElementById('username').value.trim()
    const email = document.getElementById('email').value.trim()
